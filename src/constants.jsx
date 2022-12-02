2abd867ce86faa1c8b5cd6bde06abfc9ae837271
redm3 <56494159+redm3@users.noreply.github.com>
2022-12-02 08:59:16 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x1F4c20e56d89a473d27ce6Eb7190E80166C1a0a6';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
